0f39ff1f4ada970ee3f6756c7dde9da4dfd5f0fd
Lanfig <orlandoaf@gmail.com>
2020-08-30 16:22:26 -0300

    Prompt com eco apenas

--- shell.c
+++ shell.c
@@ -1,5 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM_CMD 256
+
+int recebe_digitacao(char *entr)
+{
+	fflush(stdin);
+	if(fgets(entr, TAM_CMD, stdin) == NULL)
+	{
+		if(feof(stdin))
+		{
+			printf("fim\n");
+			exit(0);
+		}
+		fflush(stdin);
+		return 0;
+	}
+	fflush(stdin);
+
+	if(entr[0] == '\n') return 0;
+	return 1;
+}
 
 int
 main() {
+    char entrada[TAM_CMD];
+    int recebe_status;
 
+    while(1) {
+        printf("shellzin> ");
+        recebe_status = recebe_digitacao(entrada);
+        if (recebe_status == 1)
+            printf("O texto digitado foi %s", entrada);
+    }
 }
